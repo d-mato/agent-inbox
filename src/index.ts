9772720206b06b4f553a29d7c2d1835c56eb78c3
Daiki Matoba <telnetstat@gmail.com>
2026-09-07 21:50:10 +0900

    Add email handler to log incoming messages

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,13 @@ export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		return new Response("ok");
 	},
+
+	async email(message, env, ctx): Promise<void> {
+		console.log("email received", {
+			from: message.from,
+			to: message.to,
+			subject: message.headers.get("subject"),
+			size: message.rawSize,
+		});
+	},
 } satisfies ExportedHandler<Env>;
